84287670642fd2d88e839b5fe37c0448afc14f23
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-27 21:14:58 +0700

    update

---
+++ SPRINT_13_2/8_Insert_Query_Notif_Email_Registration_Hajj_Routine_Success_IDN.sql
@@ -0,0 +1,34 @@
+INSERT INTO NOTIFICATION_MANAGEMENT_SERVICE.NOTIFICATION_TEMPLATE (
+        CREATED_BY,
+        CREATED_DATE,
+        "LANGUAGE",
+        MESSAGE_SENDER,
+        MESSAGE_SUBJECT,
+        MODIFIED_BY,
+        MODIFIED_DATE,
+        TEMPLATE_CODE,
+        TEMPLATE_TYPE,
+        MESSAGE_CONTENT,
+        ICON,
+        MESSAGE_TITLE,
+        ACTION_INFO,
+        ACTION_LINK,
+        ADDITIONAL_INFO
+    )
+VALUES (
+        'SYSTEM',
+        TIMESTAMP '2023-11-27 21:14:00',
+        'IDN',
+        'Bank Syariah Indonesia<development.bsi@ist.id>',
+        'Nabung Rutin Haji Berhasil',
+        NULL,
+        NULL,
+        'HAJJ_ROUTINE_SUCCESS',
+        'EMAIL',
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    );
